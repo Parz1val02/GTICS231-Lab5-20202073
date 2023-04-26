65a47827fc791406ea4e5a00c6a318234dcaa8a2
r.b4rr10s <a20202073@pucp.edu.pe>
2023-04-25 20:16:45 -0500

    Primer Commit

---
+++ src/main/java/com/example/gtics231lab520202073/Gtics231Lab520202073Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520202073;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gtics231Lab520202073Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gtics231Lab520202073Application.class, args);
+    }
+
+}
